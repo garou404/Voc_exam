d2b7152078e85b06989605b62d5f5edc77d3bbf0
garou404 <charlesmeyer347@gmail.com>
2023-10-15 16:58:01 +0200

    premier pas dashboard

--- time spacing/app.py
+++ time spacing/app.py
@@ -1,11 +1,28 @@
-from dash import Dash, html
+from dash import Dash, html, dash_table, dcc, callback, Output, Input, State
+import pandas as pd
+import plotly.express as px
 
-# This line is known as the Dash constructor and is responsible for initializing your app. It is almost always the same for any Dash app you create.
+
+# Initialize the app
 app = Dash(__name__)
 
+# App layout
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='First app with data'),
+    html.Hr(),
+    dcc.Input(id="answer", type="text", placeholder="", style={'marginRight':'10px'}),
+    html.Button('Submit', id='submit-val'),
+    html.Div(id='correction'),
 ])
 
+# Add controls to build the interaction
+@callback(
+    Output(component_id='correction', component_property='children'),
+    Input('submit-val', 'n_clicks'),
+    State('answer', 'value'))
+def update_output(n_clicks, value):
+    return value
+
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
