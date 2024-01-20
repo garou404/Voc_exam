0de55f3843e5f6043f8dc2088a31e4c5201ef474
garou404 <charlesmeyer347@gmail.com>
2024-01-20 23:10:32 +0100

    bootstrap add

--- time spacing/app.py
+++ time spacing/app.py
@@ -5,7 +5,7 @@ import datetime
 from voc_exam import get_dataframe, upload_date, clean_data, update_row, save_series, get_series
 
  # data loading
-file = 'words_lv.xlsx'
+file = 'words_V1.0.0.xlsx'
 
 df_tempo = pd.DataFrame()
 
@@ -16,14 +16,20 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div([
         html.Div([
-            dcc.Input(id="serie-size", type="text", value=""),
-            html.Button(id='start-button', n_clicks=0, children='Start series'),
-        ], id='quiz-starter',
-        className='quiz-starter-container-div'),
             html.Div([
-        ], id='quiz-container')
-    ], className='general-quiz-container')
-], className='page-container')
+                html.Div([
+                    dcc.Input(id="serie-size", type="text", value="", className='form-control ')
+                ], className='col-md-8'),
+                html.Div([
+                    html.Button(id='start-button', n_clicks=0, children='Start series', className='btn btn-primary ')
+                ], className='col-md-4'),
+            ], id='quiz-starter',
+            className='row'),
+                html.Div([
+            ], id='quiz-container', className='row')
+        ], className='col-6 mt-5 p-5 border border-primary rounded ')
+    ], className='row justify-content-md-center pt-5')
+], className='container')
 
 @callback(Output('quiz-container', 'children'),
           Input('start-button', 'n_clicks'),
@@ -31,7 +37,7 @@ app.layout = html.Div([
           prevent_initial_call=True)
 def start_quiz(n_clicks, serie_size):
     global df_tempo
-    df_tempo = get_series(get_dataframe('words_lv.xlsx'), int(serie_size))
+    df_tempo = get_series(get_dataframe('words_V1.0.0.xlsx'), int(serie_size))
     df_tempo['asked'] = False
     df_tempo['answered'] = False
     df_tempo.iloc[0, df_tempo.columns.get_loc('asked')] = True
@@ -65,8 +71,8 @@ def display_next_question(n_clicks_right, n_clicks_wrong, answer):
     df_tempo.loc[condition, 'answered'] = True
 
     if len(df_tempo['answered'].unique()) == 1: # end of serie
-        df = get_dataframe('words_lv.xlsx')
-        save_series(df, df_tempo, 'words_lv.xlsx')
+        df = get_dataframe('words_V1.0.0.xlsx')
+        save_series(df, df_tempo, 'words_V1.0.0.xlsx')
         return html.Div(['serie complete'])
 
     result = df_tempo.loc[df_tempo['asked'] == False]
@@ -78,13 +84,31 @@ def display_next_question(n_clicks_right, n_clicks_wrong, answer):
 
 def get_quiz_layout(input_text):
     quiz_layout = html.Div([
-        html.Div(input_text),
-        dcc.Input(type='text'),
-        html.Button(id='show-answer', children='show answer'),
-        html.Div(id='answer-label'),
-        html.Button(id='right-answer-button', children='Y'),
-        html.Button(id='wrong-answer-button', children='N'),
-    ], id='quiz_layout')
+        html.Div([
+            html.Div(input_text, className='form-label'),
+        ], className='row'),
+        html.Div([
+            html.Div([
+                html.Div([
+                    dcc.Input(type='text', className='form-control'),
+                ], className='col-md-8'),
+                html.Div([
+                    html.Button(id='show-answer', children='show answer', className='btn btn-primary'),
+                ], className='col-md-4'),
+            ], className='row'),
+            html.Div([
+                html.Div(id='answer-label', className='form-label'),
+            ], className='row'),
+        ], className='row'),
+        html.Div([
+            html.Div([
+                html.Button(id='right-answer-button', children='Y', className='btn btn-success mx-2 px-5'),
+            ], className='col-md-6 d-flex justify-content-center'),
+            html.Div([
+                html.Button(id='wrong-answer-button', children='N', className='btn btn-danger mx-2 px-5' ),
+            ], className='col-md-6 d-flex justify-content-center'),
+        ], className='row'),
+    ], id='quiz_layout', className='')
     return quiz_layout
 
 # Run the app
